2b2a33fb18a0379824e3d09ffb3b92a7dc303fd1
qiuxiaochi <1348270542@qq.com>
2020-01-13 13:58:14 +0800

    小demo  不注解了

---
+++ lib/mine/MineWidget.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class MineWidget extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MineWidgetState();
+  }
+
+
+}
+
+class MineWidgetState extends State<MineWidget>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text("我的"),
+    );
+  }
+
+}
